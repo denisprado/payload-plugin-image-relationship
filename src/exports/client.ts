7810ff3dabd192f5b3940a05ffb250136378101d
Denis Prado Forigo <denisforigo@gmail.com>
2025-09-22 16:09:49 -0300

    changing extension of component

--- src/exports/client.ts
+++ src/exports/client.ts
@@ -1,4 +1,4 @@
-import ImageRelationship from '../components/Component.js'
+import ImageRelationship from '../components/Component.jsx'
 
 // Exporta o componente para substituir o campo 'relationship'
 export const relationship = {
