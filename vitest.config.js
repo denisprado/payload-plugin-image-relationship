4e7fc8b71ec71d6b6e9d69f380181bee6a6d98c2
Denis Prado Forigo <denisforigo@gmail.com>
2025-09-22 17:08:15 -0300

    Fix: Configure vitest globalSetup for MongoMemoryServer and env vars

--- vitest.config.js
+++ vitest.config.js
@@ -2,14 +2,14 @@ import path from 'path'
 import { loadEnv } from 'payload/node'
 import { fileURLToPath } from 'url'
 import { defineConfig } from 'vitest/config'
+import { MongoMemoryServer } from 'mongodb-memory-server'
 
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
 
-export default defineConfig(() => {
-  loadEnv(path.resolve(dirname, './dev'))
-  process.env.PAYLOAD_SECRET = process.env.PAYLOAD_SECRET || 'bb4a6482db6994d6c91b3e6d6427080e408f9d687b5c22cfcbc2addadc8d6fcd';
+let mongo: MongoMemoryServer
 
+export default defineConfig(() => {
   return {
     plugins: [
     ],
@@ -23,6 +23,15 @@ export default defineConfig(() => {
       hookTimeout: 60_000,
       testTimeout: 30_000,
       include: ['./dev/int.spec.ts'],
+      globalSetup: async () => {
+        mongo = await MongoMemoryServer.create()
+        process.env.DATABASE_URI = mongo.getUri()
+        process.env.PAYLOAD_SECRET = process.env.PAYLOAD_SECRET || 'bb4a6482db6994d6c91b3e6d6427080e408f9d687b5c22cfcbc2addadc8d6fcd';
+
+        return async () => {
+          await mongo.stop()
+        }
+      },
     },
   }
 })
