19eb6838311ab5de7cb61c1bb503f1fc57321410
Denis Prado Forigo <denisforigo@gmail.com>
2025-09-22 17:02:26 -0300

    Fix: Ensure PAYLOAD_SECRET is set in vitest environment

--- vitest.config.js
+++ vitest.config.js
@@ -8,6 +8,7 @@ const dirname = path.dirname(filename)
 
 export default defineConfig(() => {
   loadEnv(path.resolve(dirname, './dev'))
+  process.env.PAYLOAD_SECRET = process.env.PAYLOAD_SECRET || 'bb4a6482db6994d6c91b3e6d6427080e408f9d687b5c22cfcbc2addadc8d6fcd';
 
   return {
     plugins: [
