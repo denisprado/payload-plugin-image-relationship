cdc486674ca63bf9a32db1ff6e3d10879003653d
Denis Prado Forigo <denisforigo@gmail.com>
2025-09-22 17:20:06 -0300

    Debug: Simplify vitest.config.js to absolute minimum (again)

--- vitest.config.js
+++ vitest.config.js
@@ -1,12 +1,4 @@
-import path from 'path'
-import { fileURLToPath } from 'url'
 import { defineConfig } from 'vitest/config'
-import { MongoMemoryServer } from 'mongodb-memory-server'
-
-const filename = fileURLToPath(import.meta.url)
-const dirname = path.dirname(filename)
-
-let mongo
 
 export default defineConfig(() => {
   return {
@@ -14,20 +6,6 @@ export default defineConfig(() => {
       environment: 'node',
       hookTimeout: 60_000,
       testTimeout: 30_000,
-      include: ['./dev/int.spec.ts'],
-      globalSetup: async () => {
-        mongo = await MongoMemoryServer.create()
-        const mongoUri = mongo.getUri();
-        console.log('Mongo URI:', mongoUri);
-        process.env.DATABASE_URI = mongoUri;
-        console.log('process.env.DATABASE_URI:', process.env.DATABASE_URI);
-        process.env.PAYLOAD_SECRET = process.env.PAYLOAD_SECRET || 'bb4a6482db6994d6c91b3e6d6427080e408f9d687b5c22cfcbc2addadc8d6fcd';
-        console.log('process.env.PAYLOAD_SECRET:', process.env.PAYLOAD_SECRET);
-
-        return async () => {
-          await mongo.stop()
-        }
-      },
     },
   }
 })
