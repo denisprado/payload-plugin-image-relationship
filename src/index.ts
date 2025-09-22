fc5b9283576acbbb4cbe04d0bdfb727af6aca38f
Denis Prado Forigo <denisforigo@gmail.com>
2025-09-22 16:18:07 -0300

    changing extension of component

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,8 @@ import type { Plugin } from 'payload'
 // This makes it easy to add options in the future.
 export type ImageRelationshipOptions = Record<string, never>
 
-export const imageRelationshipPlugin =
-  (_pluginOptions?: ImageRelationshipOptions): Plugin =>
-  (incomingConfig) => {
-    // Plugin options are not used in the backend at the moment.
-    // The main functionality is client-side.
-    return incomingConfig
-  }
+export const imageRelationshipPlugin = (): Plugin => (incomingConfig) => {
+  // Plugin options are not used in the backend at the moment.
+  // The main functionality is client-side.
+  return incomingConfig
+}
